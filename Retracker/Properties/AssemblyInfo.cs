385b215c63427acf2e2b749333e401ff82cc8628
Mikhail Filippov <mikhail@filippov.me>
2015-06-08 19:54:38 +0300

    Init project

---
+++ Retracker/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Retracker")]
+[assembly: AssemblyDescription("MongoDb retracker")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mikhail Filippov")]
+[assembly: AssemblyProduct("Retracker")]
+[assembly: AssemblyCopyright("Copyright © Mikhail Filippov 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee2943a3-17ee-4f31-89f0-fbeb17ef6dbc")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
